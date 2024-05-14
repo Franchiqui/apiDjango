36980fd662fd98207f68c5d12c5e76f429954931
FranchiquiJ <167950323+FranchiquiJ@users.noreply.github.com>
2024-05-14 18:55:20 +0200

    PRIMEROS CAMBIOS CON FUNCIONES 	new file:   apps/core/utils.py 	modified:   apps/core/views.py 	modified:   config/urls.py 	modified:   requirements.txt

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .utils import scanTexto_func
+
+def extract_text_from_image(request):
+    if request.method == 'POST':
+        image_path = request.FILES['image'].temporary_file_path()
+        extracted_text = scanTexto_func(image_path)
+        return JsonResponse({'extracted_text': extracted_text})
+
 
 # Create your views here.
--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.urls import path
+from apps.core.views import extract_text_from_image
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns = [
+    path('extract-text/', extract_text_from_image, name='extract_text'),
+]
