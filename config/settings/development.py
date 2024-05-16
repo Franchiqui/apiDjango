f917f34d5bcab2ea104f0720ea3f0e81fbd9f8af
FranchiquiJ <167950323+FranchiquiJ@users.noreply.github.com>
2024-05-16 16:27:03 +0200

    f 	modified:   config/settings/development.py 	modified:   config/settings/production.py 	modified:   config/settings/settings.py

--- config/settings/development.py
+++ config/settings/development.py
@@ -4,7 +4,7 @@ from .base import *
 SECRET_KEY = "django-insecure-fyo39v-aid^2$#^_egjp^)@qi^34)&+)(#&5+2_9m26z55pk*0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['61.5.9.5', '.pythonanywhere.com']
 
